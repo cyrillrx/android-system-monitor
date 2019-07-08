fefd9b04b27df12a88f1d5cbda3be257d9ae5e8a
Cyril Leroux <cyril0leroux@gmail.com>
2019-07-08 22:31:29 +0200

    Created UserPref.kt to store stat thresholds

---
+++ app/src/main/java/com/cyrillrx/monitor/preference/UserPref.kt
@@ -0,0 +1,71 @@
+package com.cyrillrx.monitor.preference
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+
+/**
+ * @author Cyril Leroux
+ *         Created on 08/07/2019.
+ */
+object UserPref {
+
+    private const val KEY_BATTERY_THRESHOLD = "battery_threshold"
+    private const val KEY_MEMORY_THRESHOLD = "memory_threshold"
+    private const val KEY_CPU_THRESHOLD = "cpu_threshold"
+
+    private const val DEFAULT_BATTERY_THRESHOLD = 20
+    private const val DEFAULT_MEMORY_THRESHOLD = 60
+    private const val DEFAULT_CPU_THRESHOLD = 60
+
+    fun getBatteryThreshold(context: Context) = context.getPrefInt(KEY_BATTERY_THRESHOLD, DEFAULT_BATTERY_THRESHOLD)
+
+    fun saveBatteryThreshold(context: Context, threshold: Int) {
+        context.savePrefInt(KEY_BATTERY_THRESHOLD, threshold)
+    }
+
+    fun getMemoryThreshold(context: Context) = context.getPrefInt(KEY_MEMORY_THRESHOLD, DEFAULT_MEMORY_THRESHOLD)
+
+    fun saveMemoryThreshold(context: Context, threshold: Int) {
+        context.savePrefInt(KEY_MEMORY_THRESHOLD, threshold)
+    }
+
+    fun getCpuThreshold(context: Context) = context.getPrefInt(KEY_CPU_THRESHOLD, DEFAULT_CPU_THRESHOLD)
+
+    fun saveCpuThreshold(context: Context, threshold: Int) {
+        context.savePrefInt(KEY_CPU_THRESHOLD, threshold)
+    }
+
+    private fun Context.getSharedPrefs(): SharedPreferences? =
+        PreferenceManager.getDefaultSharedPreferences(applicationContext)
+
+    /**
+     * Saves an integer value into the shared preferences.
+     *
+     * @param key The name of the preference to retrieve.
+     */
+    private fun Context.savePrefInt(key: String, value: Int) {
+        getSharedPrefs()?.edit()?.putInt(key, value)?.apply()
+    }
+
+    /**
+     * Retrieves an integer value from the shared preferences.
+     * Clears the stored field if an error occurs.
+     *
+     * @param key The name of the preference to retrieve.
+     * @return The preference value if it exists, or defaultValue.
+     */
+    private fun Context.getPrefInt(key: String, defaultValue: Int = Int.MIN_VALUE): Int {
+
+        try {
+            return getSharedPrefs()?.getInt(key, defaultValue) ?: defaultValue
+        } catch (e: Exception) {
+            // If a problem occurred while parsing, better clear the stored field.
+            clearPrefKey(key)
+        }
+
+        return defaultValue
+    }
+
+    private fun Context.clearPrefKey(key: String) = getSharedPrefs()?.edit()?.remove(key)?.apply()
+}
